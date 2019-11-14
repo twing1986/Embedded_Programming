7c8d846d2936649d0280b6303e41438ef5779261
twojtasz <twojtasz@varroclighting.com>
2019-11-14 21:54:20 +0100

    Init git repository with Lesson_01: Counting

---
+++ Lesson_01/main.c
@@ -0,0 +1,24 @@
+
+int main() {
+    unsigned int counter = 0;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;
+    ++counter;  
+    ++counter;
+    ++counter;
+    ++counter;  
+    ++counter;
+    ++counter;
+    ++counter;  
+    ++counter;
+    ++counter;
+    ++counter;
+  
+  return 0;
+}
