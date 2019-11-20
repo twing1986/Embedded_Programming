f579c042b3798b9b0321afe7ce286c78abc6381c
twojtasz <twojtasz@varroclighting.com>
2019-11-20 06:23:23 +0100

    Add Lesson_02: Flow of Control

--- Lesson_02/main.c
+++ Lesson_02/main.c
@@ -6,24 +6,15 @@ Used to start with compiler & debugger (disassembly, memory, register, locals).
 int main() {
 //  int counter = 0;   // declaring a variable counter (signed integer) and initialize it to 0
     unsigned int counter = 0;   // declaring a variable
-    ++counter;  // increment the counter
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;
-    ++counter;  
-    ++counter;
-    ++counter;
-    ++counter;  
-    ++counter;
-    ++counter;
-    ++counter;  
-    ++counter;
-    ++counter;
-    ++counter;
+    
+    while (counter < 20) {
+        ++counter;  // increment the counter
+        if ((counter & 1) != 0) {
+            /* do something when the counter is odd */
+        } else {
+            /* do something when the counter is odd */
+        }
+    }
   
-  return 0;
+    return 0;
 }
