c4b44cfce088edd82de24293ded8b001230df732
twojtasz <twojtasz@varroclighting.com>
2019-11-25 13:55:50 +0100

    Add Lesson_04: Blinking the LED

---
+++ Lesson_04/main.c
@@ -0,0 +1,30 @@
+/*******************************************************************************
+Short program for blinking LED
+*******************************************************************************/
+
+int main() {
+    // Enable Clock for GPIO Port F
+    *(unsigned int *)0x400FE608U = 0x20U;        // General-Purpose Input/Output Run Mode Clock Gating Control (RCGCGPIO)
+    // Set Port F pins 1, 2 and 3 as outputs
+    *(unsigned int *)0x40025400U = 0x0EU;        // GPIO Direction (GPIODIR)
+    // Enable digital function  of PF1, PF2 and PF3
+    *(unsigned int *)0x4002551CU = 0x0EU;        // GPIO Digital Enable (GPIODEN)
+    
+    while (1) {
+        // Switch the Red LED on
+        *(unsigned int *)0x400253FCU = 0x02U;        // GPIO Data (GPIODATA)
+        // Delay
+        int counter = 0;
+        while (counter < 1000000) {
+            ++counter;
+        }    
+        // Switch the Red LED off
+        *(unsigned int *)0x400253FCU = 0x00U;
+        counter = 0;
+        while (counter < 1000000) {
+            ++counter;
+        }    
+    }
+  
+    return 0;
+}
